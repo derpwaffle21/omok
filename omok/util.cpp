79313004ec57af767a63c0e59a83580088d07cae
derpwaffle21 <derpwaffle21@gmail.com>
2022-12-31 11:29:05 +0900

    te

--- omok/util.cpp
+++ omok/util.cpp
@@ -3,7 +3,6 @@
 #include <fstream>
 #include <random>
 #include <algorithm>
-#include <cassert>
 
 #include "util.h"
 #include "types.h"
@@ -91,12 +90,6 @@ void generateRandomGame(Board& board, bool printBoard)
         if (printBoard)
             board.printBoard(true);
     }
-}
-
-void errorMessage(const char* errormessage) {
-    std::cout << errormessage << '\n';
-    assert(0);
-    return;
 }
 
 double ReLU(double x)
